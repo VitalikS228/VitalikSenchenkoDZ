b660c9ddb4f68b6a657deb59f8e77bca6932557e
VitalikS228 <119817339+VitalikS228@users.noreply.github.com>
2022-12-15 20:45:52 +0200

    adding homework4

---
+++ 4.19a.cpp
@@ -0,0 +1,27 @@
+﻿// 4.19a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include<iostream>
+
+using namespace std;
+
+//функція
+double p(int n) {
+    double result = 0;
+    for (int i = 1; i <= n; i++) {
+        result += (1 + 1 / pow(i,2));
+    }
+    return result;
+}
+//основна програма
+int main()
+{
+    int n=1;
+    //перевірка на те що N > 2
+    while (n <= 2) {
+        cout << "Enter n = ";
+        cin >> n;
+    }
+    cout << "p = " << p(n) << endl;
+}
