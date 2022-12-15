66449bc807b1541187a49e9a19aa2416e7a3a462
VitalikS228 <119817339+VitalikS228@users.noreply.github.com>
2022-12-15 20:58:44 +0200

    adding homework11

---
+++ 10.6е.cpp
@@ -0,0 +1,48 @@
+﻿// 10.6е.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//книга
+struct Book {
+    int ISBN;
+    string author;
+    string title;
+};
+
+//читач
+struct Reader {
+    int number;
+    string fio;
+    string date;
+};
+
+//бланк
+struct Blank {
+    Book book;
+    Reader reader;
+};
+
+//основна програма
+int main()
+{
+    Book book;
+    book.ISBN = 1234;
+    book.author = "W. Shekspir";
+    book.title = "Romeo and Julliet";
+
+    Reader reader = { 1, "OLeg", "2022" };
+
+    Blank blank = { book,reader };
+
+    cout << blank.book.ISBN << endl;
+    cout << blank.book.author<< endl;
+    cout << blank.book.title << endl;
+    cout << blank.reader.number<< endl;
+    cout << blank.reader.fio << endl;
+    cout << blank.reader.date << endl;
+
+}
