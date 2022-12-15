3819b5bb20b8ffb9f33e578270821451dc4cc118
VitalikS228 <119817339+VitalikS228@users.noreply.github.com>
2022-12-15 20:47:47 +0200

    adding homework5

---
+++ 5.11a.cpp
@@ -0,0 +1,44 @@
+﻿// 5.11a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include<iostream>
+
+using namespace std;
+
+//рекурсивна функція 
+double s(double a0, double a1, int k, int n) {
+    //наступний елемент
+    double ak = a1 + k * a0;
+    //кінець рекурсії
+    if (k == n){
+        return pow(2, k) * a1;
+    }
+    //коли к =1
+    else if (k == 1) {
+        k++;
+        return pow(2, k) * a0 + s(0, 1, k, n);
+    }
+    //коли к = 2
+    else if (k == 2) {
+        k++;
+        return pow(2, k) * a1 + s(0, 1, k, n);
+    }
+    //інщі випадки
+    else {
+        k++;
+        return pow(2, k) * a1 + s(a1, ak, k, n);
+    }
+}
+
+//основна програма
+int main()
+{
+    int k = 1;
+    int n;
+    cout << "Enter N = ";
+    cin >> n;
+    cout << "Sum = " << s(0,1,k,n)<< endl;
+}
---
+++ 5.16є.cpp
@@ -0,0 +1,37 @@
+﻿// 5.16є.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include<iostream>
+
+using namespace std;
+
+//рекурсивна функція 
+double y(int k, double x,double eps) {
+    double s1 = pow(-1, k) * pow(x, k);
+    k++;
+    double s2 = pow(-1, k) * pow(x, k);
+    if (s2 - s1 < eps)
+        return (s2 + s1);
+    else {
+        k++;
+        return s2 + s1 + y(k, x, eps);
+    }
+        
+}
+
+//основна програма
+int main()
+{
+    double eps = 0.001;
+    int k = 0;
+    double x;
+    
+    cout << "Enter X = ";
+    cin >> x;
+    
+    cout << "y = " << y(k, x,eps) << endl;
+    cout << "y = " << 1.0 / (1.0 + x) << endl;
+
+}
